f7791a875702d1092fff139bd6a2d45286929b0c
NurulAfifah <nurullafifahh1112@gmail.com>
2020-05-02 09:56:27 +0700

    Main Class

---
+++ src/JadwalPraktikum.java
@@ -0,0 +1,27 @@
+public class JadwalPraktikum {
+    public static void main(String[] args) {
+        Praktikum prk1 = new Praktikum();
+        prk1.haridanJam = "Jumat Pagi";
+        prk1.tanggal = "24 April 2020";
+        prk1.pertemuan = "Pertemuan 1";
+        prk1.kelas = "B Java 1 Bjb";
+        prk1.dosenPengajar = "Mirza Yogi Kurniawan";
+        prk1.cetakInfo();
+
+        Praktikum prk2 = new Praktikum();
+        prk2.haridanJam = "Sabtu pagi";
+        prk2.tanggal = "02 Mei 2020";
+        prk2.pertemuan = "Pertemuan 2";
+        prk2.kelas = "B java 1 Bjb";
+        prk2.dosenPengajar = "Mirza Yogi Kurniawan";
+        prk2.cetakInfo();
+
+        Praktikum prk3 = new Praktikum();
+        prk3.haridanJam = "Jumat Pagi";
+        prk3.tanggal = "08 Mei 2020";
+        prk3.pertemuan = "Pertemuan 3";
+        prk3.kelas = "B Java 1 Bjb";
+        prk3.dosenPengajar = "Mirza Yogi Kurniawan";
+        prk3.cetakInfo();
+    }
+}
